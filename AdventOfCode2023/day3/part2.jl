a2f339190f847db00447a58f6e0666e87a57f941
RUdall <rudall@caltech.edu>
2023-12-03 01:00:52 -0800

    separate parts 1 and 2 of day3

--- AdventOfCode2023/day3/part2.jl
+++ AdventOfCode2023/day3/part2.jl
@@ -17,18 +17,22 @@ end
 sum = 0
 for ii in eachindex(lines)
     for jj in eachindex(lines[ii])
-        if !isdigit(lines[ii][jj]) && lines[ii][jj] != '.'
+        if lines[ii][jj] == '*'
+            values = []
+            backup = copy(lines)
             for kk in -1:1
                 for ll in -1:1
                     if isdigit(lines[ii+kk][jj+ll])
                         value, line = number_from_line(lines[ii+kk], jj+ll)
-                        global sum += value
                         lines[ii+kk] = line
+                        push!(values, value)
                     end
                 end
+            end
+            global lines = backup
+            if length(values) == 2
+                global sum += values[1] * values[2]
             end
         end
     end
 end
-
-println(sum)
